7bdd81e5a23712f19a5982818a0088d7fa023fae
htmimper <htmimper@gmail.com>
2025-04-08 22:36:25 +0900

    Add files via upload

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Hello World! の出力
+		System.out.println("Hello World!");
+		System.out.println("こんにちは！！");
+		//文字列の場合は、ダブルクオテーションで囲む
+		System.out.println("例外が");
+		System.out.println("発生しました。");
+		//行の途中で改行するとエラーになるが、2行に分けて書くと改行して表示される
+		System.out.println("例外が¥r¥n発生しました。");
+		//改行コード（Windowsなら¥r¥n）を入力すると、そこで改行して表示される
+	}
+
+}
